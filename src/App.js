cf09d7764475ef66fece465bc413bb531cb2deb4
Bidisa Kalita <bidisakalita20@gmail.com>
2023-12-10 18:45:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import appname from './components/appname/appname.js'
+
+function App() {
+  return (
+    <div className="App">
+      <appname></appname>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appname/appname.js
@@ -0,0 +1,13 @@
+import React, {usestate} from 'react';
+import './appname.css';
+
+const appname = () => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default appname
